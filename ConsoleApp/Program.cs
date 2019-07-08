4379afa1e3920b41f49953cb997ace09a5895a64
Pawel Izycki <pawel.izycki@britishcouncil.pl>
2019-07-08 14:25:19 +0200

    Hosting .NET Core in ASF

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFileName = string.Format(@"C:\dev\dump\GuestExecNetCore\{0}.txt", DateTime.Now.ToLongTimeString().Replace(":", string.Empty));
+
+            while (true)
+            {
+                try
+                {
+                    File.AppendAllText(logFileName, DateTime.Now.ToLongTimeString() + "\n");
+                    Task.Delay(1000);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+    }
+}
